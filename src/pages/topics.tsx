b508e2422719df537290d808c764f915bb84b37b
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2022-02-17 23:38:28 +0800

    fix build error

--- src/pages/topics.tsx
+++ src/pages/topics.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "@/components/Layout";
-import kebabCase from "lodash.kebabCase";
+import kebabCase from "lodash.kebabcase";
 import { Link } from "gatsby";
 import { useSiteMetadata, useTagsList } from "../hooks";
 
