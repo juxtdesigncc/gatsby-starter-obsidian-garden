52e2ebfea97e47b1383d5094e0f181d7158f1131
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2022-02-23 13:48:34 +0800

    - Fix typo - Updated Features and About page - Updated post-template for Stage component

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -2,14 +2,23 @@ import React from "react";
 
 const featureData = [
   {
-    title: "Obsidian full support",
+    title: "Self-host Obsidian Website",
     description:
       "Publish your Obsidian knowledge base as a website for free using Gatsby",
   },
   {
     title: "YAML support",
+    description: "Support same YAML (e.g. tags, publish) as Obsidian",
+  },
+  {
+    title: "Notes maturity frontmatter support",
     description:
-      "Support YAML (e.g. tags, publish) that are also supported by Obsidian",
+      "Display notes' maturity, customizable stages (This template used Maggie Appleton's metaphor - Seedling, Budding, Evergreen as an example to illustrate notes' maturity)",
+  },
+  {
+    title: "SEO",
+    description:
+      "Better SEO than Obsidian Publish (Using react-helmet for basic SEO setup)",
   },
 ];
 
@@ -18,8 +27,8 @@ export default function Features() {
     <div className="grid gap-6 grid-cols-12">
       {featureData.map((item) => (
         <div className="col-span-12 md:col-span-6">
-          <span className="block w-64 h-64 bg-red-200"></span>
-          <h3 className="my-4 font-light">{item.title}</h3>
+          {/* <span className="block w-64 h-64 bg-red-200"></span> */}
+          <h2 className="my-4 font-light">{item.title}</h2>
           <p>{item.description}</p>
         </div>
       ))}
